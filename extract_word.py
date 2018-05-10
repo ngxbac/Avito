fde27abfccd4f4bd635d7866010c6804fc91fafe
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2018-05-10 09:13:44 +0900

    Update config and create a pretrained embedding
    Signed-off-by: Nguyen Xuan Bac <ngxbac.dt@gmail.com>

--- extract_word.py
+++ extract_word.py
@@ -17,6 +17,8 @@ def preprocessing(df, columns):
         df[cols] = df[cols].astype(str)
         df[cols] = df[cols].astype(str).fillna(' ')  # FILL NA
         df[cols] = df[cols].str.lower()
+        df[cols] = df[cols].str.replace("[^[:alpha:]]", " ")
+        df[cols] = df[cols].str.replace("\\s+", " ")
     return df
 
 
@@ -30,11 +32,12 @@ def main():
 
     extracted_root = config["extracted_features"]
 
-    max_features = 100000  # 50000
-    maxlen = 300
+    word_max_dict = config["word_max_dict"]
+    word_input_size = config["word_input_size"]
+    word_embedding_size = config["word_embedding_size"]
 
     with utils.timer("Load csv"):
-        # print("[+] Load csv ...")
+        print("[+] Load csv ...")
         train_df = load_csv(config["train_csv"], txt_vars)
         test_df = load_csv(config["test_csv"], txt_vars)
 
@@ -45,22 +48,54 @@ def main():
     gc.collect()
 
     with utils.timer("Pre-processing"):
-        print("[+] Preprocessing text")
+        print("\n[+] Preprocessing text")
         df = preprocessing(df, txt_vars)
+
+    with utils.timer("Create token"):
+        print("\n[+] Create token")
+        tokenizer = text.Tokenizer(num_words=word_max_dict)
+        tokenizer.fit_on_texts(df["title"].tolist() + df["description"].tolist())
 
     for txt in txt_vars:
         with utils.timer(f"Extract {txt}"):
-            print(f"[+] Extract {txt}")
-            tokenizer = text.Tokenizer(num_words=max_features)
-            tokenizer.fit_on_texts(df[txt].tolist())
+            print(f"\n[+] Extract {txt}")
             list_tokenized = tokenizer.texts_to_sequences(df[txt].tolist())
-            X_words = sequence.pad_sequences(list_tokenized, maxlen=maxlen)
+            X_words = sequence.pad_sequences(list_tokenized, maxlen=word_input_size)
+            # Split test and train
             X_train_words = X_words[:n_train, :]
             X_test_words = X_words[n_train:, :]
+            # Save the feature
             print(f"[+] Save word features of {txt}")
             utils.save_features(X_train_words, extracted_root, f"X_train_word_{txt}")
             utils.save_features(X_test_words, extracted_root, f"X_test_word_{txt}")
 
+    print("\n[+] Load pretrained embedding")
+    # Use pretrained-weights for embedding
+    EMBEDDING_FILE = config["fasttext_vec"]
+    # embed_size = 300
+    embeddings_index = {}
+    with open(EMBEDDING_FILE, encoding='utf8') as f:
+        for line in f:
+            values = line.rstrip().rsplit(' ')
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    print("[+] Load is done")
+    print("[+] Create pretrained embedding")
+    word_index = tokenizer.word_index
+    # prepare embedding matrix
+    num_words = min(word_max_dict, len(word_index) + 1)
+    embedding_matrix = np.zeros((num_words, word_embedding_size))
+    for word, i in word_index.items():
+        if i >= word_max_dict:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+    print("[+] Save pretrained embedding")
+    utils.save_features(embedding_matrix, extracted_root, "embedding_weights")
 
 if __name__ == '__main__':
     main()
