0b1b9f1d4903bd7baea1456fa3f9ef4b22c5339c
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2018-05-14 10:58:53 +0900

    Update LGBM method, adding CV
    Signed-off-by: Nguyen Xuan Bac <ngxbac.dt@gmail.com>

--- train_LGBM.py
+++ train_LGBM.py
@@ -1,126 +1,259 @@
-import json
-import pandas as pd
-import bcolz
-import numpy as np
+# Light GBM for Avito Demand Prediction Challenge
+# Uses Bag-of-Words, meta-text features, and dense features.
+# NO COMPUTER VISION COMPONENT.
+
+# https://www.kaggle.com/c/avito-demand-prediction
+# By Nick Brooks, April 2018
+
+import time
+
+notebookstart = time.time()
+
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
+import gc
+import json
 
-import lightgbm as lgb
+# print("Data:\n", os.listdir("../input"))
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader
+# Models Packages
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+from sklearn import feature_selection
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 from sklearn.model_selection import StratifiedKFold, KFold
 
-import models
-import datasets as d
-import utils
-import json
+# Gradient Boosting
+import lightgbm as lgb
 
-from scipy.sparse import hstack
+# Tf-Idf
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import FeatureUnion
+from scipy.sparse import hstack, csr_matrix
+from nltk.corpus import stopwords
 
-def load_preds_train_fold(model_name, fold):
-    file = f"pred_train_{model_name}_{fold}"
-    return utils.load_features("predict_train", file)
+# Viz
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+config = json.load(open("config.json"))
+
+print("\nData Load Stage")
+training = pd.read_csv(config["train_csv"], index_col="item_id", parse_dates=["activation_date"])
+traindex = training.index
+testing = pd.read_csv(config["test_csv"], index_col="item_id", parse_dates=["activation_date"])
+testdex = testing.index
+y = training.deal_probability.copy()
+training.drop("deal_probability", axis=1, inplace=True)
+print('Train shape: {} Rows, {} Columns'.format(*training.shape))
+print('Test shape: {} Rows, {} Columns'.format(*testing.shape))
+
+print("Combine Train and Test")
+df = pd.concat([training, testing], axis=0)
+del training, testing
+gc.collect()
+print('\nAll Data shape: {} Rows, {} Columns'.format(*df.shape))
+
+print("Feature Engineering")
+df["price"] = np.log(df["price"] + 0.001)
+df["price"].fillna(-999, inplace=True)
+df["image_top_1"].fillna(-999, inplace=True)
+
+print("\nCreate Time Variables")
+df["Weekday"] = df['activation_date'].dt.weekday
+df["Weekd of Year"] = df['activation_date'].dt.week
+df["Day of Month"] = df['activation_date'].dt.day
+
+# Create Validation Index and Remove Dead Variables
+training_index = df.loc[df.activation_date <= pd.to_datetime('2017-04-07')].index
+validation_index = df.loc[df.activation_date >= pd.to_datetime('2017-04-08')].index
+df.drop(["activation_date", "image"], axis=1, inplace=True)
+
+print("\nEncode Variables")
+categorical = ["user_id", "region", "city", "parent_category_name", "category_name", "user_type", "image_top_1"]
+print("Encoding :", categorical)
+
+# Encoder:
+lbl = preprocessing.LabelEncoder()
+for col in categorical:
+    df[col] = lbl.fit_transform(df[col].astype(str))
+
+print("\nText Features")
+
+# Feature Engineering
+df['text_feat'] = df.apply(lambda row: ' '.join([
+    str(row['param_1']),
+    str(row['param_2']),
+    str(row['param_3'])]), axis=1)  # Group Param Features
+df.drop(["param_1", "param_2", "param_3"], axis=1, inplace=True)
+
+# Meta Text Features
+textfeats = ["description", "text_feat", "title"]
+for cols in textfeats:
+    df[cols] = df[cols].astype(str)
+    df[cols] = df[cols].astype(str).fillna('nicapotato')  # FILL NA
+    df[cols] = df[cols].str.lower()  # Lowercase all text, so that capitalized words dont get treated differently
+    df[cols + '_num_chars'] = df[cols].apply(len)  # Count number of Characters
+    df[cols + '_num_words'] = df[cols].apply(lambda comment: len(comment.split()))  # Count number of Words
+    df[cols + '_num_unique_words'] = df[cols].apply(lambda comment: len(set(w for w in comment.split())))
+    df[cols + '_words_vs_unique'] = df[cols + '_num_unique_words'] / df[cols + '_num_words'] * 100  # Count Unique Words
+
+print("\n[TF-IDF] Term Frequency Inverse Document Frequency Stage")
+russian_stop = set(stopwords.words('russian'))
+
+tfidf_para = {
+    "stop_words": russian_stop,
+    "analyzer": 'word',
+    "token_pattern": r'\w{1,}',
+    "sublinear_tf": True,
+    "dtype": np.float32,
+    "norm": 'l2',
+    # "min_df":5,
+    # "max_df":.9,
+    "smooth_idf": False
+}
 
 
-def load_preds_test_fold(model_name, fold):
-    file = f"predict_root/submission_{model_name}_{fold}.csv"
-    preds = pd.read_csv(file)["deal_probability"].tolist()
-    return np.array(preds).reshape(-1, 1)
-
-def main():
-    # Load json config
-    config = json.load(open("config.json"))
-    extracted_features_root = config["extracted_features"]
-    # Load data and token len of embedding layers
-    print("[+] Load features ...")
-    y = utils.load_features(extracted_features_root, "y_train")
-    token_len = utils.load_features(extracted_features_root, "token_len")
-
-    X_train_num = utils.load_features(extracted_features_root, "X_train_num")
-    X_train_cat = utils.load_features(extracted_features_root, "X_train_cat")
-    X_train_desc = utils.load_features(extracted_features_root, "X_train_desc").any()
-    X_train_title = utils.load_features(extracted_features_root, "X_train_title").any()
-    X_train_param = utils.load_features(extracted_features_root, "X_train_param").any()
-
-    X_test_num = utils.load_features(extracted_features_root, "X_test_num")
-    X_test_cat = utils.load_features(extracted_features_root, "X_test_cat")
-    X_test_desc = utils.load_features(extracted_features_root, "X_test_desc").any()
-    X_test_title = utils.load_features(extracted_features_root, "X_test_title").any()
-    X_test_param = utils.load_features(extracted_features_root, "X_test_param").any()
-
-    # Load predict train
-    n_folds = config["n_fold"]
-    model_name = config["model_name"]
-    X_pred_train = None
-    X_pred_test = None
-    for fold in range(n_folds):
-        preds_train = load_preds_train_fold(model_name, fold)
-        preds_test = load_preds_test_fold(model_name, fold)
-        if X_pred_train is None:
-            X_pred_train = preds_train
-            X_pred_test = preds_test
-        else:
-            X_pred_train = np.concatenate((X_pred_train, preds_train), axis=1)
-            X_pred_test = np.concatenate((X_pred_test, preds_test), axis=1)
-    print("Predict train shape {}".format(X_pred_train.shape))
-    print("Predict test shape {}".format(X_pred_test.shape))
+def get_col(col_name): return lambda x: x[col_name]
 
 
-    # X = np.concatenate((X_train_num, X_train_cat, X_train_desc, X_train_title), axis=1)
-    # X = np.concatenate((X_train_num, X_train_cat, X_pred_train), axis=1)
-    X = hstack([X_train_num, X_train_cat, X_train_desc, X_train_title])
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.1)
+vectorizer = FeatureUnion([
+    ('description', TfidfVectorizer(
+        ngram_range=(1, 2),
+        max_features=18000,
+        **tfidf_para,
+        preprocessor=get_col('description'))),
+    ('text_feat', CountVectorizer(
+        ngram_range=(1, 2),
+        # max_features=7000,
+        preprocessor=get_col('text_feat'))),
+    ('title', TfidfVectorizer(
+        ngram_range=(1, 2),
+        **tfidf_para,
+        # max_features=7000,
+        preprocessor=get_col('title')))
+])
 
-    # test = np.concatenate((X_test_num, X_test_cat), axis=1)
-    # test = np.concatenate((X_test_num, X_test_cat, X_test_desc, X_test_title), axis=1)
-    test = hstack([X_test_num, X_test_cat, X_test_desc, X_test_title])
+start_vect = time.time()
+vectorizer.fit(df.loc[traindex, :].to_dict('records'))
+ready_df = vectorizer.transform(df.to_dict('records'))
+tfvocab = vectorizer.get_feature_names()
+print("Vectorization Runtime: %0.2f Minutes" % ((time.time() - start_vect) / 60))
 
-    n_rounds = 200041
+# Drop Text Cols
+df.drop(textfeats, axis=1, inplace=True)
+
+# Dense Features Correlation Matrix
+f, ax = plt.subplots(figsize=[10, 7])
+sns.heatmap(pd.concat([df.loc[traindex, [x for x in df.columns if x not in categorical]], y], axis=1).corr(),
+            annot=False, fmt=".2f", cbar_kws={'label': 'Correlation Coefficient'}, cmap="plasma", ax=ax, linewidths=.5)
+ax.set_title("Dense Features Correlation Matrix")
+plt.savefig('correlation_matrix.png')
+
+print("Modeling Stage")
+# Combine Dense Features with Sparse Text Bag of Words Features
+X = hstack([csr_matrix(df.loc[traindex, :].values), ready_df[0:traindex.shape[0]]])  # Sparse Matrix
+testing = hstack([csr_matrix(df.loc[testdex, :].values), ready_df[traindex.shape[0]:]])
+tfvocab = df.columns.tolist() + tfvocab
+for shape in [X, testing]:
+    print("{} Rows and {} Cols".format(*shape.shape))
+print("Feature Names Length: ", len(tfvocab))
+del df
+gc.collect();
+
+print("\nModeling Stage")
+
+# Training and Validation Set
+"""
+Using Randomized train/valid split doesn't seem to generalize LB score, so I will try time cutoff
+"""
+
+# Train with k-fold
+n_folds = config["n_fold"]
+skf = KFold(n_folds)
+
+for fold, (train_index, val_index) in enumerate(skf.split(X)):
+    print(f"\n[+] Fold {fold}")
+
+    X_train = X[train_index]
+    y_train = X[train_index]
+
+    X_valid = X[val_index]
+    y_valid = X[val_index]
+
+    # Save val index and test index to file
+    print(f"Save train and val index at fold {fold}")
+    file = open(f'train_val_index_fold_{fold}.txt', 'w')
+    for item in train_index:
+        file.write(f"{item} ")
+    file.write("\n")
+    for item in val_index:
+        file.write(f"{item} ")
+    file.write("\n")
+
+    file.close()
+
+
+
+    print("Light Gradient Boosting Regressor")
     lgbm_params = {
         'task': 'train',
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': 'rmse',
-        'max_depth': 13,
-        # 'num_leaves': 31,
-        'feature_fraction': 0.80,
-        # 'bagging_fraction': 0.90,
+        'max_depth': 15,
+        'num_leaves': 35,
+        'feature_fraction': 0.7,
+        'bagging_fraction': 0.8,
         # 'bagging_freq': 5,
-        'learning_rate': 0.05,
-        'lambda_l2': 5,
-        'verbose': 0,
-        "device": "gpu",
-        "max_bin": 128,
-        "num_threads": 4
+        'learning_rate': 0.019,
+        'verbose': 0
     }
 
-    lgtrain = lgb.Dataset(X_train, label=y_train)
-    lgvalid = lgb.Dataset(X_valid, label=y_valid)
-    # lgbtest = lgb.Dataset(test)
+    # LGBM Dataset Formatting
+    lgtrain = lgb.Dataset(X_train, y_train,
+                          feature_name=tfvocab,
+                          categorical_feature=categorical)
+    lgvalid = lgb.Dataset(X_valid, y_valid,
+                          feature_name=tfvocab,
+                          categorical_feature=categorical)
 
-
-    # Specify sufficient boosting iterations to reach a minimum
-    num_round = 4000
-    gpu_res = {}  # Store accuracy result
-    # Train model
+    # Go Go Go
+    modelstart = time.time()
     lgb_clf = lgb.train(
         lgbm_params,
         lgtrain,
-        num_boost_round=n_rounds,
+        num_boost_round=15000,
         valid_sets=[lgtrain, lgvalid],
         valid_names=['train', 'valid'],
         early_stopping_rounds=200,
-        verbose_eval=50
+        verbose_eval=200
     )
 
-    deal_probability = lgb_clf.predict(test)
-    submission = pd.read_csv(config["sample_submission"])
-    submission['deal_probability'] = deal_probability
-    submission['deal_probability'].clip(0.0, 1.0, inplace=True)
-    submission.to_csv(f"submission_lgbm_boost.csv", index=False)
+    print('Save model...')
+    # save model to file
+    lgb_clf.save_model(f'lgb_model_fold_{fold}.txt')
 
-if __name__ == '__main__':
-    main()
+    # Feature Importance Plot
+    f, ax = plt.subplots(figsize=[7, 10])
+    lgb.plot_importance(lgb_clf, max_num_features=50, ax=ax)
+    plt.title("Light GBM Feature Importance")
+    plt.savefig(f'feature_import_fold_{fold}.png')
+
+    print("Model Evaluation Stage")
+    print('RMSE:', np.sqrt(metrics.mean_squared_error(y_valid, lgb_clf.predict(X_valid))))
+    lgpred = lgb_clf.predict(testing)
+    lgsub = pd.DataFrame(lgpred, columns=["deal_probability"], index=testdex)
+    lgsub['deal_probability'].clip(0.0, 1.0, inplace=True)  # Between 0 and 1
+    lgsub.to_csv(f"lgb_sub_fold_{fold}.csv", index=True, header=True)
+
+    # Save out of fold
+    lgb_oof_pred = lgb_clf.predict(X_valid)
+    lgb_oof = pd.DataFrame(lgb_oof_pred, columns=["deal_probability"], index=val_index)
+    lgb_oof['deal_probability'].clip(0.0, 1.0, inplace=True)  # Between 0 and 1
+    lgb_oof.to_csv(f"lgb_oof_fold_{fold}.csv", index=True, header=True)
+
+    print("Model Runtime: %0.2f Minutes" % ((time.time() - modelstart) / 60))
+    print("Notebook Runtime: %0.2f Minutes" % ((time.time() - notebookstart) / 60))
