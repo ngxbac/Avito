02a0e3980a1c0f7845a83fb997b5b67da4b4b367
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2018-05-19 16:32:21 +0900

    Blending made score improved to 0.2200LB
    Signed-off-by: Nguyen Xuan Bac <ngxbac.dt@gmail.com>

--- blend.py
+++ blend.py
@@ -11,18 +11,19 @@ model_bag = [
         "name": "xgboost",
         "path": f"{root}/submission_xgboost.csv",
         "score": 0.2241,
-        "weight": 0.5,
+        "weight": 0.3,
     },
     {
         "name": "LightGBM",
         "path": f"{root}/submissionA5_LB02224.csv",
         "score": 0.2239,
-        "weight": 0.30,
+        "weight": 0.3,
     },
     {
         "name": "xgboost_R",
         "path": f"{root}/xgb_tfidf0.21864_02241LB.csv",
         "score": 0.2226,
+        "weight": 0.1,
     },
     # {
     #     "name": "keras_cnn",
@@ -34,7 +35,7 @@ model_bag = [
         "name": "keras_capsule",
         "path": f"{root}/keras_capsule_avg.csv",
         "score": 0.2228,
-        "weight": 0.10,
+        "weight": 0.3,
     },
     # {
     #     "name": "revert_label",
@@ -50,12 +51,13 @@ for model in model_bag:
     print("[+] Score {}".format(model["score"]))
     print("[+] path {}".format(model["path"]))
     label  = pd.read_csv(str(model["path"]))
-    preds.append(label["deal_probability"].values)
+    weight = model["weight"]
+    preds.append(weight * label["deal_probability"].values)
 
 preds = np.array(preds)
-pred_mean = np.mean(preds, axis=0)
+pred_mean = np.sum(preds, axis=0)
 
 df = pd.DataFrame()
 df['item_id'] = label['item_id']
 df['deal_probability'] = pred_mean
-df.to_csv(f'{root}/blend_3_best_models_with_xgb_.csv',index=False)
+df.to_csv(f'{root}/blend_3_best_models_with_xgb_weighted.csv',index=False)
